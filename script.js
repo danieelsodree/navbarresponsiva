48099735c42206fa0ff6eeb94808bc7775cef6f4
Daniel Sodré <danielcsodre98@gmail.com>
2022-09-07 10:42:49 -0300

    iniciando com a resposividade

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 container.addEventListener("mouseout", function(){
     container = document.getElementById("container").style.transition = "0.5s";
 })
+
+class MobileNavBar{
+    constructor(mobileMenu, navList){
+        this.mobileMenu = document.querySelector(mobileMenu);
+        this.navList = document.querySelector(navList);
+    }
+}
+
+function clicou(){
+    this.mobileMenu.addEventListener("click", function(){
+        console.log("olá");
+    })
+}
